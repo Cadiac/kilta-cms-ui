75dbc85c2ff4e9e57a96c80de9de0e9f60c94fd7
Jaakko Husso <jaakko.husso@futurice.com>
2017-05-05 01:20:40 +0300

    Add styles to events page

--- src/elm/Events/EventItem.elm
+++ src/elm/Events/EventItem.elm
@@ -9,6 +9,13 @@ import Routing exposing (eventPath, onLinkClick)
 
 import Markdown
 
+participantRow : Int -> String -> Html Msg
+participantRow index participant =
+  tr [] [
+    th [] [ text (toString index) ],
+    td [] [ text participant ]
+  ]
+
 view : EventItem -> Html Msg
 view event =
   let
@@ -16,13 +23,64 @@ view event =
       eventPath event.id
   in
     article [] [
-      hr [][],
       div [ class "heading" ] [
         a [ class "btn regular", href path, onLinkClick (Msgs.ChangeLocation path)] [
-          h2 [ class "subtitle is-4" ] [
+          h1 [ class "title" ] [
             text event.title
+          ],
+          hr [][],
+          h2 [ class "subtitle is-6" ] [
+            text ("Julkaistu " ++ event.created_on)
           ]
         ]
       ]
       , Markdown.toHtml [ class "content" ] event.text
+      , table [ class "table" ] [
+        tbody [] [
+          tr [] [
+            th [] [ text "Missä:" ],
+            td [] [ text event.location ]
+          ],
+          tr [] [
+            th [] [ text "Alku:" ],
+            td [] [ text event.event_start_time ]
+          ],
+          tr [] [
+            th [] [ text "Loppu:" ],
+            td [] [ text event.event_end_time ]
+          ],
+          tr [] [
+            th [] [ text "Ilmottautuminen alkaa:" ],
+            td [] [ text event.registration_start_time ]
+          ],
+          tr [] [
+            th [] [ text "Ilmottautuminen päättyy:" ],
+            td [] [ text event.registration_end_time ]
+          ],
+          tr [] [
+            th [] [ text "Osallistujia:" ],
+            td [] [
+              text (
+                (List.length event.participants |> toString) ++
+                "/" ++
+                (toString event.max_participants)
+              )
+            ]
+          ]
+        ]
+      ]
+      , h2 [ class "subtitle" ] [ text "Osallistujat" ]
+      , table [ class "table" ] [
+        thead [] [
+          tr [] [
+            th [] [ text "#" ],
+            th [] [ text "Nimi" ]
+          ]
+        ],
+        tbody [] (
+          List.indexedMap participantRow event.participants
+        )
+      ]
+      , hr [][]
+      , text ("Kirjoittajat: " ++ (String.join ", " event.authors))
     ]
